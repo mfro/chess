c1fc89be44a78e7fd25fb99fb1544dfe82f0a36a
Max Froehlich <max@mfro.me>
2025-01-18 17:50:46 -0800

    Update deployment

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,3 +1,5 @@
 yarn build
-scp main.js api.mfro.me:server/chess
-ssh api.mfro.me "startup/chess.sh"
+docker build -t "$MFRO_DEPLOY_REGISTRY/chess" build/
+docker push "$MFRO_DEPLOY_REGISTRY/chess"
+
+ssh "$MFRO_DEPLOY_HOST" "cd server; sudo docker compose pull; sudo docker compose up -d"
